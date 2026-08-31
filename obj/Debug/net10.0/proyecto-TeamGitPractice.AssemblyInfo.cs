67e9840173911a7b57deb309fc6c66fa05e63f9f
EduardoZabala <jhon.zabala329026@correo.itm.edu.co>
2026-08-31 18:46:05 -0500

    feat: add assigned API endpoint

--- obj/Debug/net10.0/proyecto-TeamGitPractice.AssemblyInfo.cs
+++ obj/Debug/net10.0/proyecto-TeamGitPractice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyecto-TeamGitPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20afaeed5100e70cf6b8fb47e00c55f4e2e39d06")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyecto-TeamGitPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyecto-TeamGitPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
